d842ca149a331158718dd3bd4a5ff81125fcff3a
gabrielvf1 <vf_gabriel@yahoo.com.br>
2018-06-10 18:57:16 -0300

    arrumando home

---
+++ app/src/main/java/com/fablab/insper/fablabinsper/LendoDadosHistorico.java
@@ -0,0 +1,39 @@
+package com.fablab.insper.fablabinsper;
+
+public class LendoDadosHistorico {
+
+    private String nome;
+    private  String data_dev;
+
+    public LendoDadosHistorico(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData_dev() {
+        return data_dev;
+    }
+
+    public void setData_dev(String data_dev) {
+        this.data_dev = data_dev;
+    }
+
+    public String getData_emp() {
+        return data_emp;
+    }
+
+    public void setData_emp(String data_emp) {
+        this.data_emp = data_emp;
+    }
+
+    private String data_emp;
+
+
+}
